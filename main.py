c7bf794ba9e19bf65e3cf99dcd69550ae0b8d2cc
Gokul Kannan <gokulscodes@gmail.com>
2026-03-19 12:36:44 +0530

    temp commit

--- main.py
+++ main.py
@@ -24,33 +24,43 @@ config=types.GenerateContentConfig(
         system_instruction=prompts.system_prompt
         )
 
-response = client.models.generate_content(
+for _ in range(20):
+    response = client.models.generate_content(
         model = "gemini-3-flash-preview",
         contents=messages,
         config=config
         )
 
-if response.function_calls and len(response.function_calls) > 0:
-    for function_call in response.function_calls:
-        # print(f"Calling function: {function_call.name}({function_call.args})")
-        function_call_result = call_function(function_call, args.verbose)
+    print("candidates length: ", len(response.candidates))
+    if response.candidates and len(response.candidates) > 0:
+        for candidate in response.candidates:
+            messages.append(candidate.content)
 
-        if len(function_call_result.parts) == 0:
-            raise Exception("Function call result is empty")
+    if response.function_calls and len(response.function_calls) > 0:
+        for function_call in response.function_calls:
+            # print(f"Calling function: {function_call.name}({function_call.args})")
+            function_call_result = call_function(function_call, args.verbose)
 
-        if not function_call_result.parts[0].function_response:
-            raise Exception("Function call result is missing function_response")
+            if len(function_call_result.parts) == 0:
+                raise Exception("Function call result is empty")
 
-        if not function_call_result.parts[0].function_response.response:
-            raise Exception("Function call result is missing response")
+            if not function_call_result.parts[0].function_response:
+                raise Exception("Function call result is missing function_response")
 
-        if args.verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
-else:
-    print(response.text)
+            if not function_call_result.parts[0].function_response.response:
+                raise Exception("Function call result is missing response")
 
-if response.usage_metadata is not None and args.verbose:
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+
+            messages.append(function_call_result)
+            print(messages)
+    else:
+        print(response.text)
+        break
+
+    if response.usage_metadata is not None and args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
